35610a714a575a3c4fd48779435db2f7a84f5c56
lgaubert <39573855+lgaubert@users.noreply.github.com>
2019-03-21 03:55:30 -0500

    Update app.js

--- vanillajs/js/app.js
+++ vanillajs/js/app.js
@@ -23,3 +23,16 @@
 	$on(window, 'load', setView);
 	$on(window, 'hashchange', setView);
 })();
+window.addEventListener('load', async e => {
+
+    if ('serviceWorker' in navigator) {
+        try {
+            navigator.serviceWorker.register('serviceWorker.js');
+            console.log('SW registered');
+
+        } catch (error) {
+            console.log('SW failed');
+
+        }
+    }
+});
